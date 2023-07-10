c2d5ba1ddc9573b919c86ded31f2b7f320262618
trickzhavia <patrick@zhavia.com.br>
2023-07-10 13:05:57 -0300

    added validators

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,5 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateUserDTO } from './dto/CreateUser.dto';
 import { UserRepository } from './user.repository';
 
 @Controller('/users')
@@ -6,7 +7,7 @@ export class UserController {
   constructor(private userRepository: UserRepository) {}
 
   @Post()
-  async createUser(@Body() dataUser) {
+  async createUser(@Body() dataUser: CreateUserDTO) {
     this.userRepository.save(dataUser);
   }
 
