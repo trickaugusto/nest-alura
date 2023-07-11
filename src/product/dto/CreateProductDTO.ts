4650bb967e565b7799b1a7fc1cc79a3a2fcd696c
trickzhavia <patrick@zhavia.com.br>
2023-07-11 13:08:14 -0300

    update

--- src/product/dto/CreateProductDTO.ts
+++ src/product/dto/CreateProductDTO.ts
@@ -1,36 +1,63 @@
 import { Type } from 'class-transformer';
 import {
+  ArrayMinSize,
   IsArray,
   IsDecimal,
   IsNotEmpty,
+  IsNumber,
   IsPositive,
+  IsString,
+  IsUrl,
+  MaxLength,
+  min,
+  MinLength,
   ValidateNested,
 } from 'class-validator';
 
+// https://github.com/typestack/class-validator#validation-decorators
 export class CreateProductDTO {
+  @IsString()
   @IsNotEmpty()
   nome: string;
 
-  @IsPositive({ message: 'O valor deve ser positivo' })
-  @IsDecimal({ decimal_digits: '2' })
+  @IsNumber({ maxDecimalPlaces: 2, allowNaN: false, allowInfinity: false })
   valor: string;
+
+  @IsNumber()
   quantidade: number;
+
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(1000)
   descricao: string;
 
   @ValidateNested()
-  @IsArray()
+  @ArrayMinSize(3)
   @Type(() => CaracteristicaProdutoDTO)
   caracteristicas: CaracteristicaProdutoDTO[];
+
+  @ArrayMinSize(3)
   imagens: ImagemProdutoDTO[];
+
+  @IsNotEmpty()
   categoria: string;
 }
 
 class CaracteristicaProdutoDTO {
+  @IsString()
+  @IsNotEmpty()
   nome: string;
+
+  @IsString()
+  @IsNotEmpty()
   descricao: string;
 }
 
 class ImagemProdutoDTO {
+  @IsUrl()
   url: string;
+
+  @IsString()
+  @IsNotEmpty()
   descricao: string;
 }
